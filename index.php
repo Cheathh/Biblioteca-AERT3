0905622b0addf71bc15290a62884c274568106f2
Cheath <61672154+Cheathh@users.noreply.github.com>
2021-06-16 16:36:30 +0100

    Update all without erros | bugs
    commit 4

--- index.php
+++ index.php
@@ -2,20 +2,20 @@
 
 session_start();
 $serverName = "(local)";
-$connectionInfo = array("Database"=>"mydatabase","UID"=>"myusername", "PWD"=>"mypassword");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
-if( $conn === false){
+$mysqli = new mysqli( "localhost", "root", "", "formulario");
+if( $mysqli === false){
     echo "Error in connection.\n";
-    die( print_r( sqlsrv_errors(), true));
+    die( print_r( $mysqli->error(), true));
 }
-$username = $_REQUEST['uNm'];
-$password  = $_REQUEST['uPw'];
-$tsql = "SELECT * FROM li WHERE uNm='$username' AND uPw='$password'";
-$stmt = sqlsrv_query( $conn, $tsql, array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+@$username = $_REQUEST['username'];
+@$password  = $_REQUEST['password'];
+$tsql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+echo $tsql;
+$stmt = $mysqli -> query($tsql);
 if($stmt == true){
     $_SESSION['valid_user'] = true;
     $_SESSION['uNm'] = $username;
-    header('Location: index.php');
+    header('Location: login.php');
     die();
 }
 ?>
